2c2a5f767b9c088e20bc3949231b2faf30c81320
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2025-02-20 22:59:57 +0300

    Added Task1

---
+++ Homework1/src/Task1/ColumnPrinting.java
@@ -0,0 +1,12 @@
+package Task1;
+
+public class ColumnPrinting {
+    public static void main(String[] args) {
+        printTreeWords();
+
+    }
+
+    public static void printTreeWords() {
+        System.out.println("Orange\n" + "Banana\n" + "Apple\n");
+    }
+}
