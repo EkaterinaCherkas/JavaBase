5d6265e035854ca808d11de80410357b88d0d2d0
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2025-02-22 14:08:06 +0300

    Added Task4

---
+++ Homework1/src/Task4/NumComparison.java
@@ -0,0 +1,18 @@
+package Task4;
+
+public class NumComparison {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers(){
+        int a = 33;
+        int b = 24;
+        if (a >= b){
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
